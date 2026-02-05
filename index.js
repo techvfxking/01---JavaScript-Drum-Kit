e80452eb34a2b84c348187ae5e91f68f5239225e
biplabCyberSwift <jobs.biplab.sharma@gmail.com>
2026-02-06 00:31:31 +0530

    Keyboard sound key function added

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const keyboardKeysArray = Array.from(document.querySelectorAll(".key"));
 const main = () => {
+    registerDivClickEvents();
+    registerKeyboardKeyDown();
+};
+
+const registerKeyboardKeyDown = () => {
+    document.addEventListener("keydown", e => { playSound(e.keyCode.toString()) });
+}
+
+const registerDivClickEvents = () => {
+    const keyboardKeysArray = Array.from(document.querySelectorAll(".key"));
     for (let i = 0; i < keyboardKeysArray.length; i++) {
         const keyboardKeyElement = keyboardKeysArray[i];
         keyboardKeyElement.addEventListener("click", (e) => { playSound(e.currentTarget.dataset.key, e.currentTarget) });
         keyboardKeyElement.addEventListener('transitionend', (e) => { removeTransition(e) });
     }
+}
 
-};
-
-const playSound = (key, element) => {
+const playSound = (key, element = undefined) => {
     const audioElement = document.querySelector(`audio[data-key="${key}"]`);
     if (audioElement === undefined || audioElement === null) return;
 
+    if (element === undefined) {
+        element = document.querySelector(`div[data-key="${key}"]`);
+    }
     element.classList.add("key-press");
+
     audioElement.currentTime = 0;
     audioElement.play();
 }
